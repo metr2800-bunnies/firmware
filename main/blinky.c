69fe711ddb80197a682efc993af6b031340df5c8
wilszdev <83633335+wilszdev@users.noreply.github.com>
2025-04-03 20:12:36 +1000

    use newer timer api

--- main/blinky.c
+++ main/blinky.c
@@ -1,44 +1,57 @@
-#include "driver/timer.h"
+#include "driver/gptimer.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 
 #define LED_GPIO GPIO_NUM_2
 
-/* timer interrupt configuration constants */
-#define CPU_TIMER_FREQ_HZ           80000000
-#define TIMER_DIVIDER               80
-#define TIMER_FREQ_HZ_NUMERATOR     1
+/* timer config constants */
+#define TIMER_RESOLUTION_HZ         1000000
+#define TIMER_FREQ_HZ_NUMERATOR     5
 #define TIMER_FREQ_HZ_DENOMINATOR   2
 
+static gptimer_handle_t gptimer = 0;
 volatile uint8_t state = 0;
 
-bool IRAM_ATTR timer_interrupt(void* arg) {
+static bool
+IRAM_ATTR timer_interrupt(gptimer_handle_t timer,
+        const gptimer_alarm_event_data_t *eventData, void *userData)
+{
     state ^= 1;
     gpio_set_level(LED_GPIO, state);
     return true;
 }
 
-void timer_setup() {
-    timer_config_t config = {
-        .alarm_en = TIMER_ALARM_EN,
-        .counter_en = TIMER_PAUSE,
-        .intr_type = TIMER_INTR_LEVEL,
-        .counter_dir = TIMER_COUNT_UP,
-        .auto_reload = TIMER_AUTORELOAD_EN,
-        .divider = TIMER_DIVIDER
+void
+timer_setup(void)
+{
+    gptimer_config_t timerConfig = {
+        .clk_src = GPTIMER_CLK_SRC_DEFAULT,
+        .direction = GPTIMER_COUNT_UP,
+        .resolution_hz = TIMER_RESOLUTION_HZ,
     };
-    timer_init(TIMER_GROUP_0, TIMER_0, &config);
-    timer_set_counter_value(TIMER_GROUP_0, TIMER_0, 0);
-    uint64_t alarmValue = ((CPU_TIMER_FREQ_HZ / TIMER_DIVIDER)
-            * TIMER_FREQ_HZ_DENOMINATOR / TIMER_FREQ_HZ_NUMERATOR);
-    timer_set_alarm_value(TIMER_GROUP_0, TIMER_0, alarmValue);
-    timer_enable_intr(TIMER_GROUP_0, TIMER_0);
-    timer_isr_callback_add(TIMER_GROUP_0, TIMER_0, timer_interrupt, NULL, 0);
-    timer_start(TIMER_GROUP_0, TIMER_0);
+    ESP_ERROR_CHECK(gptimer_new_timer(&timerConfig, &gptimer));
+
+    gptimer_alarm_config_t alarmConfig = {
+        .alarm_count = TIMER_RESOLUTION_HZ * TIMER_FREQ_HZ_DENOMINATOR
+            / TIMER_FREQ_HZ_NUMERATOR,
+        .reload_count = 0,
+        .flags.auto_reload_on_alarm = true,
+    };
+    ESP_ERROR_CHECK(gptimer_set_alarm_action(gptimer, &alarmConfig));
+
+    gptimer_event_callbacks_t callbacks = {
+        .on_alarm = timer_interrupt,
+    };
+    ESP_ERROR_CHECK(gptimer_register_event_callbacks(gptimer, &callbacks, 0));
+
+    ESP_ERROR_CHECK(gptimer_enable(gptimer));
+    ESP_ERROR_CHECK(gptimer_start(gptimer));
 }
 
-void app_main() {
+void
+app_main(void)
+{
     gpio_reset_pin(LED_GPIO);
     gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
     timer_setup();
