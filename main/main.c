d86b43cd645d001af3e6208b3a0398a4020611d2
wilszdev <83633335+wilszdev@users.noreply.github.com>
2025-05-08 18:20:01 +1000

    start BLE with ESP-IDF example project

--- main/main.c
+++ main/main.c
@@ -1,213 +1,263 @@
-#include "driver/i2c_master.h"
-#include "driver/gptimer.h"
-#include "driver/gpio.h"
 #include "esp_log.h"
-#include "freertos/FreeRTOS.h"
-#include "led_strip.h"
-#include "quadrature_encoder.h"
-#include "tb6612fng.h"
-#include "mpu6050.h"
+#include "nvs_flash.h"
+#include "main.h"
 
-/* ENCODER GPIO */
-#define ENCODER1A_GPIO  GPIO_NUM_12
-#define ENCODER1B_GPIO  GPIO_NUM_11
-#define ENCODER2A_GPIO  GPIO_NUM_14
-#define ENCODER2B_GPIO  GPIO_NUM_13
-#define ENCODER3A_GPIO  GPIO_NUM_47
-#define ENCODER3B_GPIO  GPIO_NUM_21
-#define ENCODER4A_GPIO  GPIO_NUM_43
-#define ENCODER4B_GPIO  GPIO_NUM_44
+/* BLE */
+#include "nimble/nimble_port.h"
+#include "nimble/nimble_port_freertos.h"
+#include "host/ble_hs.h"
+#include "host/util/util.h"
+#include "console/console.h"
+#include "services/gap/ble_svc_gap.h"
 
-/* MOTOR GPIO */
-#define MOTOR1_IN1_GPIO GPIO_NUM_7
-#define MOTOR1_IN2_GPIO GPIO_NUM_6
-#define MOTOR1_PWM_GPIO GPIO_NUM_5
-#define MOTOR2_IN1_GPIO GPIO_NUM_15
-#define MOTOR2_IN2_GPIO GPIO_NUM_16
-#define MOTOR2_PWM_GPIO GPIO_NUM_17
-#define MOTOR3_IN1_GPIO GPIO_NUM_39
-#define MOTOR3_IN2_GPIO GPIO_NUM_37
-#define MOTOR3_PWM_GPIO GPIO_NUM_36
-#define MOTOR4_IN1_GPIO GPIO_NUM_40
-#define MOTOR4_IN2_GPIO GPIO_NUM_41
-#define MOTOR4_PWM_GPIO GPIO_NUM_42
+static const char *tag = "BLE";
+static int bleprph_gap_event(struct ble_gap_event *event, void *arg);
+static uint8_t own_addr_type;
 
-/* I2C GPIO */
-#define SDA_GPIO        GPIO_NUM_1
-#define SCL_GPIO        GPIO_NUM_4
-#define I2C_PORT_NUM    I2C_NUM_0
-
-/* LIMIT SWITCH GPIO */
-#define LIM1_GPIO   GPIO_NUM_45
-#define LIM2_GPIO   GPIO_NUM_35
-#define LIM3_GPIO   GPIO_NUM_9
-#define LIM4_GPIO   GPIO_NUM_10
-
-/* SERVO GPIO */
-#define SERVO1_GPIO     GPIO_NUM_18
-#define SERVO2_GPIO     GPIO_NUM_8
-#define SERVO3_GPIO     GPIO_NUM_46
-
-/* PUSHBUTTON GPIO */
-#define BOOT_BUTTON_GPIO    GPIO_NUM_0
-
-/* LED GPIO */
-#define LED_GPIO            GPIO_NUM_38
-#define LED_STRIP_LENGTH    1
-#define LED_BRIGHTNESS      0.05    // neopixel LEDs are bright man
-
-#define TIMER_RESOLUTION_HZ         1000000
-#define TIMER_FREQ_HZ_NUMERATOR     1
-#define TIMER_FREQ_HZ_DENOMINATOR   4
-
-static gptimer_handle_t gptimer = 0;
-static led_strip_handle_t led_strip;
-static volatile uint8_t state = 0;
-
+/**
+ * Enables advertising with the following parameters:
+ *     o General discoverable mode.
+ *     o Undirected connectable mode.
+ */
 static void
-configure_led(void)
+bleprph_advertise(void)
 {
-    led_strip_config_t strip_config = {
-        .strip_gpio_num = LED_GPIO,
-        .max_leds = LED_STRIP_LENGTH,
-        .led_model = LED_MODEL_WS2812,
-        .color_component_format = LED_STRIP_COLOR_COMPONENT_FMT_GRB,
-        .flags.invert_out = false,
+    struct ble_gap_adv_params adv_params;
+    struct ble_hs_adv_fields fields;
+    const char *name;
+    int rc;
+
+    /**
+     *  Set the advertisement data included in our advertisements:
+     *     o Flags (indicates advertisement type and other general info).
+     *     o Advertising tx power.
+     *     o Device name.
+     *     o 16-bit service UUIDs (alert notifications).
+     */
+
+    memset(&fields, 0, sizeof fields);
+
+    /* Advertise two flags:
+     *     o Discoverability in forthcoming advertisement (general)
+     *     o BLE-only (BR/EDR unsupported).
+     */
+    fields.flags = BLE_HS_ADV_F_DISC_GEN |
+                   BLE_HS_ADV_F_BREDR_UNSUP;
+
+    /* Indicate that the TX power level field should be included; have the
+     * stack fill this value automatically.  This is done by assigning the
+     * special value BLE_HS_ADV_TX_PWR_LVL_AUTO.
+     */
+    fields.tx_pwr_lvl_is_present = 1;
+    fields.tx_pwr_lvl = BLE_HS_ADV_TX_PWR_LVL_AUTO;
+
+    name = ble_svc_gap_device_name();
+    fields.name = (uint8_t *)name;
+    fields.name_len = strlen(name);
+    fields.name_is_complete = 1;
+
+    fields.uuids16 = (ble_uuid16_t[]) {
+        BLE_UUID16_INIT(GATT_SVR_SVC_ALERT_UUID)
     };
-    led_strip_rmt_config_t rmt_config = {
-        .clk_src = RMT_CLK_SRC_DEFAULT,
-        .resolution_hz = 10 * 1000 * 1000,
-        .mem_block_symbols = 0,
-        .flags.with_dma = true,
-    };
-    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
-}
+    fields.num_uuids16 = 1;
+    fields.uuids16_is_complete = 1;
 
-static bool IRAM_ATTR
-timer_interrupt(gptimer_handle_t timer,
-        const gptimer_alarm_event_data_t *eventData, void *userData)
-{
-    state = (state + 1) % 3;
-    return true;
-}
+    rc = ble_gap_adv_set_fields(&fields);
+    if (rc != 0) {
+        MODLOG_DFLT(ERROR, "error setting advertisement data; rc=%d\n", rc);
+        return;
+    }
 
-static void
-i2c_setup(void)
-{
-    i2c_config_t config = {
-        .mode = I2C_MODE_MASTER,
-        .sda_io_num = SDA_GPIO,
-        .sda_pullup_en = GPIO_PULLUP_ENABLE,
-        .scl_io_num = SCL_GPIO,
-        .scl_pullup_en = GPIO_PULLUP_ENABLE,
-        .master.clk_speed = 400000,
-    };
-
-    ESP_ERROR_CHECK(i2c_param_config(I2C_PORT_NUM, &config));
-    ESP_ERROR_CHECK(i2c_driver_install(I2C_PORT_NUM, config.mode, 0, 0, 0));
-}
-
-mpu6050_handle_t mpu6050 = NULL;
-
-static void
-mpu6050_setup(void)
-{
-    // this other library (https://github.com/PiotrTopa/esp32-MPU6050) seems to support using DMP, which would be
-    // kinda nice if we hadn't run out of GPIOs already.
-
-    mpu6050 = mpu6050_create(I2C_PORT_NUM, MPU6050_I2C_ADDRESS);
-    ESP_ERROR_CHECK(mpu6050_config(mpu6050, ACCE_FS_16G, GYRO_FS_2000DPS)); // probably don't need full range
-
-    uint8_t id = 0;
-    ESP_ERROR_CHECK(mpu6050_get_deviceid(mpu6050, &id));
-
-    if (id == MPU6050_WHO_AM_I_VAL) {
-        ESP_LOGI("main", "ID checks out");
-    } else {
-        ESP_LOGE("main", "Bad ID: %d", (int)id);
+    /* Begin advertising. */
+    memset(&adv_params, 0, sizeof adv_params);
+    adv_params.conn_mode = BLE_GAP_CONN_MODE_UND;
+    adv_params.disc_mode = BLE_GAP_DISC_MODE_GEN;
+    rc = ble_gap_adv_start(own_addr_type, NULL, BLE_HS_FOREVER,
+                           &adv_params, bleprph_gap_event, NULL);
+    if (rc != 0) {
+        MODLOG_DFLT(ERROR, "error enabling advertisement; rc=%d\n", rc);
+        return;
     }
 }
 
-static void
-timer_setup(void)
+/**
+ * The nimble host executes this callback when a GAP event occurs.  The
+ * application associates a GAP event callback with each connection that forms.
+ * bleprph uses the same callback for all connections.
+ *
+ * @param event                 The type of event being signalled.
+ * @param ctxt                  Various information pertaining to the event.
+ * @param arg                   Application-specified argument; unused by
+ *                                  bleprph.
+ *
+ * @return                      0 if the application successfully handled the
+ *                                  event; nonzero on failure.  The semantics
+ *                                  of the return code is specific to the
+ *                                  particular GAP event being signalled.
+ */
+static int
+bleprph_gap_event(struct ble_gap_event *event, void *arg)
 {
-    gptimer_config_t timerConfig = {
-        .clk_src = GPTIMER_CLK_SRC_DEFAULT,
-        .direction = GPTIMER_COUNT_UP,
-        .resolution_hz = TIMER_RESOLUTION_HZ,
-    };
-    ESP_ERROR_CHECK(gptimer_new_timer(&timerConfig, &gptimer));
+    struct ble_gap_conn_desc desc;
+    int rc;
 
-    gptimer_alarm_config_t alarmConfig = {
-        .alarm_count = TIMER_RESOLUTION_HZ * TIMER_FREQ_HZ_DENOMINATOR / TIMER_FREQ_HZ_NUMERATOR,
-        .reload_count = 0,
-        .flags.auto_reload_on_alarm = true,
-    };
-    ESP_ERROR_CHECK(gptimer_set_alarm_action(gptimer, &alarmConfig));
+    switch (event->type) {
+    case BLE_GAP_EVENT_CONNECT:
+        /* A new connection was established or a connection attempt failed. */
+        MODLOG_DFLT(INFO, "connection %s; status=%d ",
+                    event->connect.status == 0 ? "established" : "failed",
+                    event->connect.status);
+        if (event->connect.status == 0) {
+            rc = ble_gap_conn_find(event->connect.conn_handle, &desc);
+            assert(rc == 0);
+        }
+        MODLOG_DFLT(INFO, "\n");
 
-    gptimer_event_callbacks_t callbacks = {
-        .on_alarm = timer_interrupt,
-    };
-    ESP_ERROR_CHECK(gptimer_register_event_callbacks(gptimer, &callbacks, 0));
+        if (event->connect.status != 0) {
+            /* Connection failed; resume advertising. */
+            bleprph_advertise();
+        }
+        return 0;
 
-    ESP_ERROR_CHECK(gptimer_enable(gptimer));
-    ESP_ERROR_CHECK(gptimer_start(gptimer));
+    case BLE_GAP_EVENT_DISCONNECT:
+        MODLOG_DFLT(INFO, "disconnect; reason=%d ", event->disconnect.reason);
+        MODLOG_DFLT(INFO, "\n");
+
+        /* Connection terminated; resume advertising. */
+        bleprph_advertise();
+        return 0;
+
+    case BLE_GAP_EVENT_CONN_UPDATE:
+        /* The central has updated the connection parameters. */
+        MODLOG_DFLT(INFO, "connection updated; status=%d ",
+                    event->conn_update.status);
+        rc = ble_gap_conn_find(event->conn_update.conn_handle, &desc);
+        assert(rc == 0);
+        MODLOG_DFLT(INFO, "\n");
+        return 0;
+
+    case BLE_GAP_EVENT_ADV_COMPLETE:
+        MODLOG_DFLT(INFO, "advertise complete; reason=%d",
+                    event->adv_complete.reason);
+        bleprph_advertise();
+        return 0;
+
+    case BLE_GAP_EVENT_NOTIFY_TX:
+        MODLOG_DFLT(INFO, "notify_tx event; conn_handle=%d attr_handle=%d "
+                    "status=%d is_indication=%d",
+                    event->notify_tx.conn_handle,
+                    event->notify_tx.attr_handle,
+                    event->notify_tx.status,
+                    event->notify_tx.indication);
+        return 0;
+
+    case BLE_GAP_EVENT_SUBSCRIBE:
+        MODLOG_DFLT(INFO, "subscribe event; conn_handle=%d attr_handle=%d "
+                    "reason=%d prevn=%d curn=%d previ=%d curi=%d\n",
+                    event->subscribe.conn_handle,
+                    event->subscribe.attr_handle,
+                    event->subscribe.reason,
+                    event->subscribe.prev_notify,
+                    event->subscribe.cur_notify,
+                    event->subscribe.prev_indicate,
+                    event->subscribe.cur_indicate);
+        return 0;
+
+    case BLE_GAP_EVENT_MTU:
+        MODLOG_DFLT(INFO, "mtu update event; conn_handle=%d cid=%d mtu=%d\n",
+                    event->mtu.conn_handle,
+                    event->mtu.channel_id,
+                    event->mtu.value);
+        return 0;
+
+    default:
+        /* The default behaviour for the event is to reject authorize request */
+        event->authorize.out_response = BLE_GAP_AUTHORIZE_REJECT;
+        return 0;
+    }
+
+    return 0;
 }
 
-static quadrature_encoder_handle_t encoders[4] = {};
-
 static void
-encoder_setup(void)
+bleprph_on_reset(int reason)
 {
-    ESP_ERROR_CHECK(quadrature_encoder_init());
-    ESP_ERROR_CHECK(quadrature_encoder_create(ENCODER1A_GPIO, ENCODER1B_GPIO, &encoders[0]));
-    ESP_ERROR_CHECK(quadrature_encoder_create(ENCODER2A_GPIO, ENCODER2B_GPIO, &encoders[1]));
-    ESP_ERROR_CHECK(quadrature_encoder_create(ENCODER3A_GPIO, ENCODER3B_GPIO, &encoders[2]));
-    ESP_ERROR_CHECK(quadrature_encoder_create(ENCODER4A_GPIO, ENCODER4B_GPIO, &encoders[3]));
+    MODLOG_DFLT(ERROR, "Resetting state; reason=%d\n", reason);
 }
 
-static tb6612fng_channel_handle_t motors[4] = {};
-
 static void
-motor_setup(void)
+bleprph_on_sync(void)
 {
-    ESP_ERROR_CHECK(tb6612fng_init());
-    ESP_ERROR_CHECK(tb6612fng_channel_create(MOTOR1_IN1_GPIO, MOTOR1_IN2_GPIO, MOTOR1_PWM_GPIO, &motors[0]));
-    ESP_ERROR_CHECK(tb6612fng_channel_create(MOTOR2_IN1_GPIO, MOTOR2_IN2_GPIO, MOTOR2_PWM_GPIO, &motors[1]));
-    ESP_ERROR_CHECK(tb6612fng_channel_create(MOTOR3_IN1_GPIO, MOTOR3_IN2_GPIO, MOTOR3_PWM_GPIO, &motors[2]));
-    ESP_ERROR_CHECK(tb6612fng_channel_create(MOTOR4_IN1_GPIO, MOTOR4_IN2_GPIO, MOTOR4_PWM_GPIO, &motors[3]));
+    int rc;
+
+    /* Make sure we have proper identity address set (public preferred) */
+    rc = ble_hs_util_ensure_addr(0);
+    assert(rc == 0);
+
+    /* Figure out address to use while advertising (no privacy for now) */
+    rc = ble_hs_id_infer_auto(0, &own_addr_type);
+    if (rc != 0) {
+        MODLOG_DFLT(ERROR, "error determining address type; rc=%d\n", rc);
+        return;
+    }
+
+    /* Printing ADDR */
+    uint8_t addr_val[6] = {0};
+    rc = ble_hs_id_copy_addr(own_addr_type, addr_val, NULL);
+
+    MODLOG_DFLT(INFO, "Device Address: ");
+    MODLOG_DFLT(INFO, "\n");
+    /* Begin advertising. */
+    bleprph_advertise();
+}
+
+void bleprph_host_task(void *param)
+{
+    ESP_LOGI(tag, "BLE Host Task Started");
+    /* This function will return only when nimble_port_stop() is executed */
+    nimble_port_run();
+
+    nimble_port_freertos_deinit();
 }
 
 void
 app_main(void)
 {
-    configure_led();
-    timer_setup();
-    encoder_setup();
-    motor_setup();
-    i2c_setup();
-    mpu6050_setup();
+    int rc;
 
-    tb6612fng_channel_set_drive(motors[0], TB6612FNG_CCW, 0.2f);
-    tb6612fng_channel_set_drive(motors[1], TB6612FNG_CW, 0.4f);
-    tb6612fng_channel_set_drive(motors[2], TB6612FNG_CCW, 0.6f);
-    tb6612fng_channel_set_drive(motors[3], TB6612FNG_CW, 0.8f);
+    /* Initialize NVS — it is used to store PHY calibration data */
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
 
-    uint8_t last_state = 255;
+    ret = nimble_port_init();
+    if (ret != ESP_OK) {
+        ESP_LOGE(tag, "Failed to init nimble %d ", ret);
+        return;
+    }
+    /* Initialize the NimBLE host configuration. */
+    ble_hs_cfg.reset_cb = bleprph_on_reset;
+    ble_hs_cfg.sync_cb = bleprph_on_sync;
+    ble_hs_cfg.gatts_register_cb = gatt_svr_register_cb;
+    ble_hs_cfg.store_status_cb = ble_store_util_status_rr;
 
-    while (1) {
-        if (last_state != state) {
-            last_state = state;
-            uint8_t r, g, b;
-            switch (state) {
-                case 0: r = (uint8_t)(255 * LED_BRIGHTNESS); g = 0; b = 0; break;
-                case 1: r = 0; g = (uint8_t)(255 * LED_BRIGHTNESS); b = 0; break;
-                case 2: r = 0; g = 0; b = (uint8_t)(255 * LED_BRIGHTNESS); break;
-                default: r = g = b = 0; break;
-            }
-            ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, 0, r, g, b));
-            ESP_ERROR_CHECK(led_strip_refresh(led_strip));
-        }
-        vTaskDelay(10 / portTICK_PERIOD_MS);
+    ble_hs_cfg.sm_io_cap = BLE_SM_IO_CAP_NO_IO;
+    ble_hs_cfg.sm_sc = 0;
+    rc = gatt_svr_init();
+    assert(rc == 0);
+
+    /* Set the default device name. */
+    rc = ble_svc_gap_device_name_set("nimble-bleprph");
+    assert(rc == 0);
+
+    nimble_port_freertos_init(bleprph_host_task);
+
+    /* Initialize command line interface to accept input from user */
+    rc = scli_init();
+    if (rc != ESP_OK) {
+        ESP_LOGE(tag, "scli_init() failed");
     }
 }
